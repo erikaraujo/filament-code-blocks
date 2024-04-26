1da6ef3ef09b4d640220855d211d3ddb5118d1b7
Erik Araujo <contact@erikaraujo.com>
2024-04-26 06:46:57 -0300

    Implements copy functionality

--- resources/views/infolists/components/code-block-entry.blade.php
+++ resources/views/infolists/components/code-block-entry.blade.php
@@ -1,4 +1,11 @@
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
+    @php
+        $copyableState = $getCopyableState($state) ?? $state;
+        $copyMessage = $getCopyMessage($state);
+        $copyMessageDuration = $getCopyMessageDuration($state);
+        $itemIsCopyable = $isCopyable($state);
+    @endphp
+
     @if(! $getShouldUseInlineTheme())
         @push('styles')
             <link href="{{ asset('css/tempest/highlight/' . $getTheme() .'.css') }}" rel="stylesheet">
@@ -12,7 +19,18 @@
         @endpush
     @endif
 
-    <div>
+    <div
+        @if($itemIsCopyable)
+            style="cursor: pointer;"
+            x-on:click="
+                window.navigator.clipboard.writeText(@js($copyableState))
+                $tooltip(@js($copyMessage), {
+                    theme: $store.theme,
+                    timeout: @js($copyMessageDuration),
+                })
+            "
+        @endif
+    >
         <pre class='filament-code-block'><code>{{ $getState() }}</code></pre>
     </div>
 </x-dynamic-component>
