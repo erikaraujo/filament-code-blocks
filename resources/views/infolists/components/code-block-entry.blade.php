f029db4a975aaebacd71a1b023db365a77a756d8
Erik Araujo <contact@erikaraujo.com>
2024-04-26 06:16:29 -0300

    Adds style

--- resources/views/infolists/components/code-block-entry.blade.php
+++ resources/views/infolists/components/code-block-entry.blade.php
@@ -2,6 +2,13 @@
     @if(! $getShouldUseInlineTheme())
         @push('styles')
             <link href="{{ asset('css/tempest/highlight/' . $getTheme() .'.css') }}" rel="stylesheet">
+            <style>
+                pre.filament-code-block {
+                    white-space: pre-wrap;
+                    padding: 1rem;
+                    border-radius: 0.5rem;
+                }
+            </style>
         @endpush
     @endif
 
